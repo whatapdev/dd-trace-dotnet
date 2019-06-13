0df1f5e60c18c8a340b045fa4d8b1ad9118d475b
Zach Montoya <zach.montoya@datadoghq.com>
2019-06-13 16:04:05 -0700

    Docker Compose changes to allow the windows integration tests to share the dependencies of the linux integration tests.

---
+++ docker/Datadog.Trace.ClrProfiler.IntegrationTests.StartDependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euxo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+$DIR/with-profiler-logs.bash \
+    wait-for-it redis:6379 -- \
+    wait-for-it elasticsearch6:9200 -- \
+    wait-for-it elasticsearch5:9205 -- \
+    wait-for-it sqlserver:1433 -- \
+    wait-for-it mongo:27017 -- \
+    wait-for-it postgres:5432
